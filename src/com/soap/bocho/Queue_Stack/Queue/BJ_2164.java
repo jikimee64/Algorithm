7372632047e431fdca5962cc0a0cdc7e4ba15226
WooCheol Kim <jikimee64@gmail.com>
2021-01-20 19:17:33 +0900

    [add] 보초 스택,큐 실습문제

---
+++ src/com/soap/bocho/Queue_Stack/Queue/BJ_2164.java
@@ -0,0 +1,29 @@
+package com.soap.bocho.Queue_Stack.Queue;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+//https://www.acmicpc.net/problem/2164
+public class BJ_2164 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int N = in.nextInt();
+
+        Queue q = new LinkedList();
+        for(int i = 1; i <= N; i++){
+            q.add(i);
+        }
+
+        while(true){
+            if(q.size() <= 1){
+                break;
+            }
+            q.poll();
+            q.add(q.poll());
+        }
+
+        System.out.println(q.poll());
+
+    }
+}
