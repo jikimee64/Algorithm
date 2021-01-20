760ba87a6355e8a144866d6378e434ec4e0eeb26
WooCheol Kim <jikimee64@gmail.com>
2021-01-20 15:12:41 +0900

    [add] 백준 LCS / 동빈북 Part3 DP 금광, 정수삼각형, 퇴사

---
+++ src/com/soap/ndb/dp/NDB_Part03_금광.java
@@ -0,0 +1,57 @@
+package com.soap.ndb.dp;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class NDB_Part03_금광 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < T; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int n = Integer.parseInt(st.nextToken());
+            int m = Integer.parseInt(st.nextToken());
+
+            int[][] arr = new int[n][m];
+            int [][]d = new int[n][m];
+
+            StringTokenizer st2 = new StringTokenizer(br.readLine());
+            for (int j = 0; j < n; j++) {
+                for (int k = 0; k < m; k++) {
+                    arr[j][k] = Integer.parseInt(st2.nextToken());
+                }
+            }
+
+            //첫번째 열 초기화
+            for (int z = 0; z < n; z++) {
+                d[z][0] = arr[z][0];
+            }
+
+            for (int w = 1; w < m; w++) { //열
+                for (int a = 0; a < n; a++) { //행
+
+                    if (a == 0) { //1번째 줄
+                        d[a][w] += arr[a][w] + Math.max(d[a][w - 1], d[a + 1][w - 1]); //왼쪽, 왼쪽 아래
+                    } else if (a == n - 1) { //마지막줄
+                        d[a][w] += arr[a][w] + Math.max(d[a][w - 1], d[a - 1][w - 1]); //왼쪽, 왼쪽 위
+                    } else { //중간 줄
+                        d[a][w] += arr[a][w] + Math.max(d[a - 1][w - 1], Math.max(d[a][w - 1], d[a + 1][w - 1])); //왼쪽 위, 왼쪽, 왼쪽 아래
+                    }
+                }
+            }
+
+            int answer = 0;
+            for (int y = 0; y < n; y++) {
+                answer = Math.max(answer, d[y][m - 1]);
+            }
+            System.out.println(answer);
+
+        } //T
+
+    }
+}
---
+++ src/com/soap/ndb/dp/NDB_Part03_정수삼각형.java
@@ -0,0 +1,55 @@
+package com.soap.ndb.dp;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+//https://www.acmicpc.net/problem/1932
+public class NDB_Part03_정수삼각형 {
+
+    public static int[][] tri;
+    public static int[][] d;
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int answer = 0;
+
+        tri = new int[N][N];
+        d = new int[N][N];
+
+        //값 삽입
+        for(int i = 0; i < N; i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for(int j = 0; j <= i; j++){
+                tri[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+
+        d[0][0] = tri[0][0];
+        for(int i = 1; i < tri.length; i++){
+            int end = tri[i].length;
+            for(int j = 0; j < end; j++){
+                if(j == 0){//처음
+                    d[i][j] = d[i-1][j] + tri[i][j];
+                }else if(j == end-1){//끝
+                    d[i][j] = d[i-1][j-1] + tri[i][j];
+                }else{//중간
+                    d[i][j] = Math.max(d[i-1][j-1] + tri[i][j],
+                            d[i-1][j] + tri[i][j]);
+                }
+            }
+        }
+
+        for(int i = 0; i < d[N-1].length; i++){
+            answer = Math.max(answer, d[N-1][i]);
+        }
+
+        System.out.println(answer);
+
+    }
+}
