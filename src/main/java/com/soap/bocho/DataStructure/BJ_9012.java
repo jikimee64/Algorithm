fc6e5bd88d09bb6c147a6fad4e5b0abbb5586821
WooCheol <jikimee64@gmail.com>
2021-08-21 01:09:24 +0900

    [Refactor, Add] 보초 자료구조 스택(괄호) / 큐 2

--- src/main/java/com/soap/bocho/DataStructure/BJ_9012.java
+++ src/main/java/com/soap/bocho/DataStructure/BJ_9012.java
@@ -16,7 +16,7 @@ public class BJ_9012 {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringBuffer sb = new StringBuffer();
+        StringBuilder sb = new StringBuilder();
 
         StringTokenizer st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken());
@@ -26,56 +26,30 @@ public class BJ_9012 {
             StringTokenizer st2 = new StringTokenizer(br.readLine());
             List<String> pars = Arrays.asList(st2.nextToken().split(""));
 
-            if (pars.size() % 2 != 0) {
-                sb.append(FAIL + "\n");
-            } else {
-                int size = pars.size() / 2;
-                while (size-- > 0) {
-                    boolean remove = pars.remove("()");
-                    System.out.println(remove);
-                }
-
-                if(pars.size() == 0){
-                    sb.append(SUCCESS + "\n");
-                }else{
-                    sb.append(FAIL + "\n");
+            for (String par : pars) {
+                if ("(".equals(par)) {
+                    stack.add(par);
+                } else if (stack.isEmpty()) {
+                    sb.append(FAIL);
+                    break;
+                } else {
+                    stack.pop();
                 }
             }
 
-//            int left = 0;
-//            int right = 0;
-//
-//            stack.addAll(pars);
-//
-//            for (String str : stack) {
-//                if ("(".equals(str)) {
-//                    left++;
-//                } else {
-//                    right++;
-//                }
-//            }
-//
-//            if (left == right) {
-//                for (int k = 0; k < stack.size(); k++) {
-//                    String pop = stack.pop();
-//                    if(pop.equals(")")){
-//
-//                    }
-//
-//                }
-//                sb.append(SUCCESS + "\n");
-//            } else {
-//                sb.append(FAIL + "\n");
-//            }
+            if (sb.length() == 0) {
+                if (stack.isEmpty()) {
+                    sb.append(SUCCESS);
+                } else {
+                    sb.append(FAIL);
+                }
+            }
 
-            bw.write(sb.toString());
+            bw.write(sb.toString() + "\n");
             bw.flush();
             sb.delete(0, sb.length());
-//            left = 0;
-//            right = 0;
             stack.clear();
         }
-
         br.close();
         bw.close();
     }
