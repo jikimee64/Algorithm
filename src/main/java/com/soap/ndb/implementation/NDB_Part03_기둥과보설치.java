713bb131258e5bf3a76f0bc3c8e088745f49b850
jikimee64 <jikimee64@gmail.com>
2023-01-15 20:37:09 +0900

    git init
    - 2023 새롭게 시작하기 위한 초기화

--- src/main/java/com/soap/ndb/implementation/NDB_Part03_기둥과보설치.java
+++
@@ -1,5 +0,0 @@
-package main.java.com.soap.ndb.implementation;
-
-//https://programmers.co.kr/learn/courses/30/lessons/60061
-public class NDB_Part03_기둥과보설치 {
-}
--- src/main/java/com/soap/woowacourse/jyami_precourse/baseball/Main.java
+++
@@ -1,9 +0,0 @@
-package com.soap.woowacourse.jyami_precourse.baseball;
-
-import com.soap.woowacourse.jyami_precourse.baseball.gameRunnber.GameMachine;
-
-public class Main {
-    public static void main(String[] args) {
-        new GameMachine().runBaseBallGameOnce();
-    }
-}
