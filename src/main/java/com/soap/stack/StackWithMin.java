6477f30d216f9d9ea74ae06c2e9e45245bfdeac0
jikimee64 <jikimee64@gmail.com>
2023-01-22 14:12:18 +0900

    Feat: 스택의 작은값 찾기(버전1,2) 진행중

---
+++ src/main/java/com/soap/stack/StackWithMin.java
@@ -0,0 +1,21 @@
+//package com.soap.stack;
+//
+//import lombok.Getter;
+//
+//import java.util.Stack;
+//
+//public class StackWithMin {
+//
+//    public static void main(String[] args) {
+//        StackWithMin stack = new StackWithMin();
+//        stack.push(3);
+//        stack.push(5);
+//        stack.push(1);
+//        stack.push(2);
+//        System.out.println("min: " + stack.min()); // 1
+//        System.out.println(stack.pop().value); // 2
+//        System.out.println(stack.pop().value); // 1
+//        System.out.println("min: " + stack.min()); // 3
+//    }
+//
+//}
---
+++ src/main/java/com/soap/stack/StackWithMinV2.java
@@ -0,0 +1,19 @@
+//package com.soap.stack;
+//
+//public class StackWithMinV2
+//
+//    public static void main(String[] args) {
+//        StackWithMinV2 stack = new StackWithMinV2();
+//        stack.push(3);
+//        stack.push(5);
+//        stack.push(1);
+//        stack.push(2);
+//        System.out.println("min: " + stack.min()); // 1
+//        System.out.println("pop: " + stack.pop()); // 2
+//        System.out.println("min: " + stack.min()); // 1
+//        System.out.println("pop: " + stack.pop()); // 1
+//        System.out.println("min: " + stack.min()); // 3
+//        System.out.println("pop: " + stack.pop()); // 5
+//        System.out.println("min: " + stack.min()); // 3
+//    }
+//}
