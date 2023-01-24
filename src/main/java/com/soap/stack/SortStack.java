f3a3c3f50f061d1578e71f948fea6afc3cb0a60f
WooCheol <jikimee64@gmail.com>
2023-01-24 21:34:05 +0900

    Feat: Stack 정렬하기

--- src/main/java/com/soap/stack/SortStack.java
+++ src/main/java/com/soap/stack/SortStack.java
@@ -1,18 +1,33 @@
-//package com.soap.stack;
-//
-//public class SortStack {
-//
-//    public static void main(String[] args) {
-//        Stack<Integer> s1 = new Stack<>();
-//        s1.push(3);
-//        s1.push(5);
-//        s1.push(1);
-//        s1.push(6);
-//        sort(s1);
-//        System.out.println(s1.pop()); // 1
-//        System.out.println(s1.pop()); // 3
-//        System.out.println(s1.pop()); // 5
-//        System.out.println(s1.pop()); // 6
-//    }
-//
-//}
+package com.soap.stack;
+
+public class SortStack {
+
+    public static void main(String[] args) {
+        Stack<Integer> s1 = new Stack<>();
+        s1.push(3);
+        s1.push(5);
+        s1.push(1);
+        s1.push(6);
+        sort(s1);
+        System.out.println(s1.pop()); // 1
+        System.out.println(s1.pop()); // 3
+        System.out.println(s1.pop()); // 5
+        System.out.println(s1.pop()); // 6
+    }
+
+    private static void sort(Stack<Integer> s1) {
+        Stack<Integer> s2 = new Stack<>();
+        while (!s1.isEmpty()) {
+            Integer pop = s1.pop();
+            while (!s2.isEmpty() && s2.peek() > pop) {
+                s1.push(s2.pop());
+            }
+            s2.push(pop);
+        }
+
+        while (!s2.isEmpty()) {
+            s1.push(s2.pop());
+        }
+    }
+
+}
