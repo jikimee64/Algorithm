9da9578a6b1231bf7387006e31a7dcb65412f8f1
WooCheol <jikimee64@gmail.com>
2023-02-04 00:49:30 +0900

    Feat: Stack bottom,top 포인터 진행중

---
+++ src/main/java/com/soap/stack/SetOfStacks2.java
@@ -0,0 +1,57 @@
+//package com.soap.stack;
+//
+//public class SetOfStacks2 {
+//
+//    public static void main(String[] args) {
+//
+//    }
+//
+//}
+//
+//class Stack<E> {
+//
+//    int capacity; //스택에 쌓을 수 있는 한계점
+//    int size; // 현재 스택의 사이즈
+//    Node top;
+//    Node bottom;
+//
+//    public Stack(int capacity) {
+//        this.capacity = capacity;
+//    }
+//
+//    public boolean isEmpty() {
+//        return size == 0;
+//    }
+//
+//    public boolean isFull() {
+//        return size == capacity;
+//    }
+//
+//    public int size() {
+//        return size;
+//    }
+//
+//    public void push(E d) {
+//    }
+//
+//    public void push(Node n) {
+//    }
+//
+//    public E pop() {
+//    }
+//
+//    public Node popBottom() {
+//    }
+//
+//    class Node {
+//        E data;
+//        Node below;
+//        Node above;
+//
+//        public Node(E data) {
+//            this.data = data;
+//        }
+//    }
+//
+//
+//}
