c9897040d6418cf6b90cd383a8572c3ba3f33754
jikimee64 <jikimee64@gmail.com>
2023-02-13 19:03:39 +0900

    Feat: 스택 올바른 괄호 진행중

---
+++ src/main/java/com/soap/stack/Bracket.java
@@ -0,0 +1,15 @@
+package com.soap.stack;
+
+//https://school.programmers.co.kr/learn/courses/30/lessons/12909?language=java
+public class Bracket {
+    public static void main(String[] args) {
+        String s = "(())()";
+        System.out.println(solution(s));
+    }
+
+    public static boolean solution(String s){
+        boolean answer = true;
+
+        return answer;
+    }
+}
