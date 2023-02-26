95d8a6d25e719f4a560c3552cef0942b832ab5f5
jikimee64 <jikimee64@gmail.com>
2023-02-26 12:09:02 +0900

    Feat: 단방향 Linked List 중간노드 삭제

--- src/main/java/com/soap/linkedlist/LinkedListNode.java
+++ src/main/java/com/soap/linkedlist/LinkedListNode.java
@@ -88,6 +88,13 @@ class LinkedList {
      * 처음과 마지막 노드는 지우지 못함
      */
     boolean deleteNode(Node n) {
+        if(n == null || n.next == null){
+            return false;
+        }
+
+        n.data = n.next.data;
+        n.next = n.next.next;
+
         return true;
     }
 
